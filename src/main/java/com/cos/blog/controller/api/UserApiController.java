cf01168d7d0c16c3791c4f329682506bbb01ff59
Suno7526 <119337968+Suno7526@users.noreply.github.com>
2023-12-31 01:30:33 +0900

    로그인 추가

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -11,16 +11,18 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.service.UserService;
 
+import jakarta.servlet.http.HttpSession;
+
+
 @RestController
 public class UserApiController {
 
-
-
-	@Autowired 
+	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private HttpSession session;
 
-	
-	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) {
 		System.out.println("UserApiController : save");
@@ -28,5 +30,16 @@ public class UserApiController {
 		userService.회원가입(user);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
-	
+
+	@PostMapping("/api/user/login")
+	public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) {
+		System.out.println("UserApiController : login");
+		User principal = userService.로그인(user);
+
+		if (principal != null) {
+			session.setAttribute("principal", principal);
+		}
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+	}
+
 }
