76537cd0cd22152f371a5c227f49817bbb2af187
Suno7526 <119337968+Suno7526@users.noreply.github.com>
2024-01-06 00:29:28 +0900

    카카오 토큰 컨트롤러 추가

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -1,8 +1,16 @@
 package com.cos.blog.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 import com.cos.blog.config.auth.PrincipalDetail;
 
@@ -17,6 +25,34 @@ public class UserController {
 	@GetMapping("/auth/loginForm")
 	public String loginForm() {
 		return "user/loginForm";
+	}
+	
+	@GetMapping("/auth/kakao/callback")
+	public @ResponseBody String kakaoCallback(String code) {
+		
+		
+		
+		RestTemplate rt = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+		
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type","authorization_code");
+		params.add("client_id","bb2b05edf7345796320799a167ba2732");
+		params.add("redirect","http://localhost:8000/auth/kakao/callback");
+		params.add("code",code);
+		
+		HttpEntity<MultiValueMap<String,String>> kakaoTokenRequest =
+				new HttpEntity<>(params, headers);
+		
+		ResponseEntity<String> response = rt.exchange(
+				"https://kauth.kakao.com/oauth/token",
+				HttpMethod.POST,
+				kakaoTokenRequest,
+				String.class
+				);
+		
+		return"카카오 인증 완료 : 토큰요청에 대한 응답 : "+response;
 	}
 	
 	@GetMapping("/user/updateForm")
